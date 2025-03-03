e9ccce7dec0c3bf1e97f4eba01384a200486b41d
polyalents <pavlinpolya66@gmail.com>
2025-03-03 05:28:10 +0300

    ..

--- bot.py
+++ bot.py
@@ -1,8 +1,6 @@
 import os
 import asyncio
 import nest_asyncio
-from multiprocessing import Process
-from flask import Flask
 from dotenv import load_dotenv
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
@@ -12,12 +10,6 @@ load_dotenv()
 
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 WEB3_URL = os.getenv("WEB3_URL", "https://hermesx.ru")
-
-app = Flask(__name__)
-
-@app.route("/")
-def index():
-    return "Hello from HermesX bot with Flask!"
 
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     keyboard = [[InlineKeyboardButton("Открыть миниапп Web3", url=WEB3_URL)]]
@@ -32,16 +24,8 @@ async def run_telegram_bot():
     application = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
     application.add_handler(CommandHandler("start", start_command))
     application.add_handler(CommandHandler("help", help_command))
-    # Передаём disable_signals=True для избежания регистрации сигналов
+    # disable_signals=True безопасно, если бот запускается в отдельном процессе
     await application.run_polling(disable_signals=True)
 
-def start_bot():
-    asyncio.run(run_telegram_bot())
-
 if __name__ == "__main__":
-    # Запускаем Telegram-бот в отдельном процессе
-    bot_process = Process(target=start_bot)
-    bot_process.start()
-    
-    # Запускаем Flask-приложение
-    app.run(host="0.0.0.0", port=8000)
+    asyncio.run(run_telegram_bot())
